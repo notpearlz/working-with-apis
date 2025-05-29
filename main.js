df20b29d12dad8447e355b1602a4e76030fadac6
Duy <duypersonal512@gmail.com>
2025-05-28 21:37:08 -0500

    async and wait

--- main.js
+++ main.js
@@ -12,17 +12,17 @@ form.addEventListener("submit", (e)=>{
     e.preventDefault();
 
     if(validSearch){
-        fetch(
-            "https://api.giphy.com/v1/gifs/translate?api_key=tIzb3AThmrdJciOYx1YVs7LuogxMzFQC&s=" + search.value,
-            { mode: "cors" }
-            )
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (response) {
-                img.src = response.data.images.original.url;
-            });
-
+        searchGif();
     }
 
 })
+
+
+
+
+const searchGif =  async () => {
+    const response = await fetch("https://api.giphy.com/v1/gifs/translate?api_key=tIzb3AThmrdJciOYx1YVs7LuogxMzFQC&s=" + search.value, { mode: "cors" });
+    const searchData = await response.json();
+    
+    img.src = searchData.data.images.original.url;
+}
